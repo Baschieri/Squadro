3479fa39adf2c45e49cce8f5a4064473dd5b7f06
JacopoBandoni <62346082+JacopoBandoni@users.noreply.github.com>
2020-03-18 16:48:58 +0100

    Add files via upload

---
+++ Squadro/src/iia/simpleGame/squadro/PawnPosition.java
@@ -0,0 +1,12 @@
+package iia.simpleGame.squadro;
+
+public class PawnPosition {
+
+	Integer position;
+	Boolean forward;
+	
+	public PawnPosition( Integer position) 
+	{
+		this.position = position;
+	}
+}
